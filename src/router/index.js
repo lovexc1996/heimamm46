aeab99ddcdc3fdd88457904f06c2cb45c3a90343
lovexc1996 <574307551@qq.com>
2020-02-08 10:55:26 +0800

    登录模块-表单布局-输入框

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ Vue.use(VueRouter)
 const router = new VueRouter({
     routes: [
         {
-            path: '/login',
+            path: '/',
             component: login
         },
         
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -8,23 +8,34 @@
         <span class="line"></span>
         <span class="sub-title">用户登录</span>
       </div>
-      <div class="inputAre">
-        <el-input
-          class="userInput"
-          placeholder="请输入用户名"
-          prefix-icon="el-icon-user"
-          v-model="username"
-        ></el-input>
-        <el-input
-          class="pswInput"
-          show-password
-          placeholder="请输入密码"
-          prefix-icon="el-icon-lock"
-          v-model="password"
-        ></el-input>
-        <el-input class="pskInput" placeholder="请输入验证码" prefix-icon="el-icon-key" v-model="passkey"></el-input>
-      </div>
-      
+      <el-form ref="form" :model="loginform" label-width="43px">
+        <!-- 手机号 -->
+        <el-form-item>
+          <el-input placeholder="请输入手机号" prefix-icon="el-icon-user" v-model="loginform.phone"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item>
+          <el-input
+            show-password
+            placeholder="请输入密码"
+            prefix-icon="el-icon-lock"
+            v-model="loginform.password"
+          ></el-input>
+        </el-form-item>
+        <!-- 验证码 -->
+        <el-form-item>
+          <el-input placeholder="请输入验证码" prefix-icon="el-icon-key" v-model="loginform.logincode"></el-input>
+        </el-form-item>
+        <!-- 用户协议 -->
+        <el-form-item>
+          <el-checkbox v-model="loginform.isChecked">是否同意用户协议</el-checkbox>
+        </el-form-item>
+        <!-- 登录按钮 -->
+        <el-form-item>
+          <el-button type="primary">登录</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
     </div>
     <img src="../../assets/login_banner_ele.png" alt />
   </div>
@@ -34,9 +45,12 @@
 export default {
   data() {
     return {
-      username: "",
-      password: "",
-      passkey: ""
+      loginform: {
+        phone: "",
+        password: "",
+        logincode: "",
+        isChecked: false
+      }
     };
   },
   //组件的名字
@@ -61,11 +75,15 @@ export default {
     width: 478px;
     background-color: #f5f5f5;
     height: 550px;
+    // 设置样式 把表单间隙空出来
+    padding-right: 41px;
+    box-sizing: border-box;
     .title-box {
       display: flex;
       align-items: center;
       margin-top: 44px;
       margin-left: 48px;
+      margin-bottom: 27px;
       .logo {
       }
       .title {
@@ -87,24 +105,6 @@ export default {
         font-family: PingFangSC;
         font-weight: 400;
         color: rgba(12, 12, 12, 1);
-      }
-    }
-    .inputAre {
-      margin-top: 29px;
-      margin-left: 43px;
-      .userInput {
-        margin-bottom: 25px;
-        width: 394px;
-        background: rgba(255, 255, 255, 1);
-      }
-      .pswInput {
-        margin-bottom: 25px;
-        width: 394px;
-        background: rgba(255, 255, 255, 1);
-      }
-      .pskInput {
-        width: 284px;
-        background: rgba(255, 255, 255, 1);
       }
     }
   }
